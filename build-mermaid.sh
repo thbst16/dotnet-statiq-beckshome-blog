307689912281254e7d4920dc75b25ce9ede7b219
Thomas Beck <thomas@beckshome.com>
2022-09-26 09:26:51 +0000

    added gantt chart to mermaid blog post

--- build-mermaid.sh
+++ build-mermaid.sh
@@ -1,4 +1,5 @@
 pushd input/img/mermaid
 ../../../node_modules/.bin/mmdc -i flowchart.mmd -o flowchart.svg
 ../../../node_modules/.bin/mmdc -i sequence.mmd -o sequence.svg
+../../../node_modules/.bin/mmdc -i gantt.mmd -o gantt.svg
 popd
